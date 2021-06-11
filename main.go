614451f6df7ca5894ea62d838ee3e5061f2192dd
Frixuu <kontakt@lukasz.xyz>
2021-06-11 12:18:43 +0200

    Allow uploading files

--- main.go
+++ main.go
@@ -1,13 +1,58 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	router := gin.Default()
+	router.MaxMultipartMemory = 8 << 20 // 8 MiB
+
+	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run()
+
+	router.POST("/v1/upload", func(c *gin.Context) {
+
+		file, err := c.FormFile("artifact")
+		if err != nil {
+			log.Println(err)
+			c.String(http.StatusBadRequest, fmt.Sprintf("Error while uploading: %s", err))
+			return
+		}
+
+		tempDir, err := ioutil.TempDir("", "bearpush-")
+		if err != nil {
+			log.Println(err)
+			c.String(http.StatusInternalServerError,
+				"Could not create a temporary directory for artifact. Check logs for details.")
+			return
+		}
+
+		err = c.SaveUploadedFile(file, path.Join(tempDir, "artifact"))
+		if err != nil {
+			log.Printf("Cannot save artifact: %s", err)
+			c.String(http.StatusInternalServerError,
+				"Could not save the uploaded artifact. Check logs for details.")
+			return
+		}
+
+		err = os.RemoveAll(tempDir)
+		if err != nil {
+			log.Printf("Cannot remove temporary directory %s: %s", tempDir, err)
+		}
+
+		c.String(http.StatusOK, "test")
+	})
+
+	router.Run()
 }
